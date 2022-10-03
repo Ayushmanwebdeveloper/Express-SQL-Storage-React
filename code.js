d6cb73935906a57ac9a98334cb7f9c84cd3f7197
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2022-10-04 00:11:46 +0530

    update code.js

--- code.js
+++ code.js
@@ -16,6 +16,7 @@ field.addEventListener("change", function () {
   // And save the results into the session storage object
   sessionStorage.setItem("autosave", field.value);
 });
+
 /*Check whether localStorage contains a data item called bgcolor
 using getItem().
 If localStorage contains bgcolor, run a function called setStyles()
@@ -47,3 +48,105 @@ const setStyles = () => {
   pElem.style.fontFamily = currentFont;
   imgElem.setAttribute("src", currentImage);
 };
+
+/*How to create a cookie in Javascript
+As you've seen before, the document interface represents
+the web page loaded in a user's browser.
+Since cookies are stored on a user's browser, it makes sense that the
+document object also allows you to get/set cookies on a user's browser:*/
+
+const firstCookie = "favoriteCat=million";
+document.cookie = firstCookie;
+const secondCookie = "favoriteDog=bambi";
+document.cookie = secondCookie;
+document.cookie; // Returns "favoriteCat=million; favoriteDog=bambi"
+
+/*Deleting a cookie in JavaScript
+You can delete cookies using JavaScript by setting
+a cookie's expiration date
+to a date in the past, causing them to expire:*/
+
+const firstCookie1 = "favoriteCat=million";
+document.cookie = firstCookie1;
+document.cookie; // Returns "favoriteCat=million"
+// specify the cookies "name" (the key) with an "="
+// and set the expiration date to the past
+document.cookie = "favoriteCat=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+document.cookie; // ""
+const { prototype } = require("events");
+///////////////////////////////////////////////////////////////////
+
+const express = require("express");
+
+// Create the Express app.
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+port = 3000;
+app.get("/login", (req, res) => {});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+/////////////////////////////////////
+// The application will send the same response to any GET request
+//  that matches the string or expressions in the array routes.
+let paths = ["/", "/ab+cd", "/a/"];
+
+app.get(paths, (req, res) => {
+  // Send response
+});
+/////////////////////////////////////
+/*The callback function is called by Express whenever an incoming request
+matches the route. The function defines two parameters, req and res,
+giving you access respectively to the Request object and the Response
+object. The Request (req) object is used to get details about the
+client request that's currently being processed.
+The Response (res) object is used to prepare a response to return to
+the client.*/
+/*The res object also supports sending JSON back to the client through
+the .json() method:*/
+
+app.get('/json', (req, res) => {
+  const resp = {
+    property1: "value1",
+    property2: "value2",
+  };
+  res.json(resp); //This will call JSON.stringify() on the input to res.json()
+})
+/*1-JSON.stringify(value) will turn the value passed into it into a JSON string.
+2-JSON.parse(str) will turn a JSON-formatted string into a JavaScript object.*/
+/*3-res.json() It sends a JSON response. This method is identical
+to res.send() when an object or array is passed, but it also converts
+non-objects to json.*/
+
+// There is no actual difference between res.send and res.json,
+// both methods are almost identical. res.json calls res.send at the end.
+// When you have an object or array which you have to pass as a response
+// then you can use any one of them.
+
+// But the main difference between res.json and res.send comes into picture
+// when you have to pass non objects as a response. res.json will convert
+// non objects (ex. null, undefined etc) as well which are actually not
+//a valid JSON by calling JSON.stringify() whereas res.send will not convert them.
+
+//////////////////////////////////////////////////////////
+const data = {
+  users: [],
+};
+
+// Use POST to add a user
+app.post("/users", (req, res) => {
+  let newUser = {
+    name: "Phyllis",
+    age: 68,
+  };
+  data.users.push(newUser);
+  res.send(newUser);
+});
+
+// Use GET to retrieve a list of all users
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+//////////////////////////////////////////////////////////
