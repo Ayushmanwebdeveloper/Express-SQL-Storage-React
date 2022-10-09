9851d4d9e03ecfe5438c85118dbdb2e451d028a5
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2022-10-09 20:40:53 +0530

    update code.js

--- code.js
+++ code.js
@@ -1636,6 +1636,160 @@ For example, if you try to modify a column with a UNIQUE constraint.
 UPDATE friends
 SET id = 2
 WHERE id = 1;
-Because id is a primary key and the new value for the id is already taken, you will see a message in SQLite that it already exists:
+Because id is a primary key and the new value for the id is already taken,
+you will see a message in SQLite that it already exists:
 
 Error: UNIQUE constraint failed: friends.id`;
+///////////////////////////////////////////////////////////////////////////
+//SQL and Express
+`Begin with Express boilerplate
+
+// Instantiate Express and the application
+const express = require('express');
+const app = express();
+
+// Process environment variables
+require('dotenv').config();
+
+// Instantiate database here
+// ...
+
+// Express using json
+app.use(express.json());
+
+// Routes will go here
+// ...
+
+// Set port and listen for incoming requests
+const port = 5000;
+app.listen(5000, () => console.log('Server is listening on port', port));
+To connect to a SQLite3 database, you can use the sqlite3 package.
+Install it by running npm install sqlite3 in your Terminal.
+
+In the Express application code near the beginning of the file,
+place the setup code for the database constants. It should be BEFORE
+any Express middleware.
+
+// Instantiate SQLite and database
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(
+    process.env.data_source,
+    sqlite3.OPEN_READWRITE
+);
+
+Tip: Using a constant for the database name, if used, should only be a
+temporary part of the code. Environment variables are the right place to
+put database connection information (e.g. process.env.data_source).
+It is a best practice to limit the Express server permissions on the database.
+Using sqlite3.OPEN_READWRITE means that the Express server can read and write
+to the database (that is, SELECT, INSERT, UPDATE, and DELETE).
+However, it will NOT be able to CREATE or DROP tables.
+///////////////////////////////////////////////////////////////////////////
+Query for all records
+The most basic SQL statement is a simple SELECT. It is useful to begin
+with one to ensure the connection to the database is fully operational.
+
+Define a route.
+
+app.get('/trees', (req, res, next) => {
+    // route handling goes here
+));
+Inside the route handler, query the database.
+
+const sql = 'SELECT * FROM trees';
+const params = [];
+
+db.all(sql, params, (err, rows) => {
+    // response handling goes here
+});
+The first argument in any of the database callback functions is err
+which holds the error, if any, and the second parameter is the result
+of the function call.
+
+When using db.all(), the result is an array of rows.
+
+Finish the route handler by responding with the rows in a json response
+format.
+
+app.get('/trees', (req, res, next) => {
+    const sql = 'SELECT id, name FROM trees';
+    const params = [];
+
+    db.all(sql, params, (err, rows) => {
+          res.json(rows);
+    });
+});
+Following these best practices, a route to return all records in a database
+table looks like this:
+
+app.get('/trees', (req, res, next) => {
+    const sql = 'SELECT id, name FROM trees';
+    const params = [];
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            next(err);
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+The query for 1 row looks quite similar. Notice the function is different
+(db.all() for all records, and db.get() for one record). Also, you'll see
+there is a question mark (?) in the SQL statement where a dynamic value needs
+to go. In a route handler, the value usually comes as part of the route's
+path so will be in req.params.
+
+Here's a complete example with the route handler, SQL statement, params,
+database query and response handling.
+
+app.get('/trees/:id', (req, res, next) => {
+    const sql = 'SELECT * FROM trees WHERE id = ?';
+    const params = [req.params.id];
+
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          next(err);
+        } else {
+          res.json(row);
+        }
+    });
+});
+
+Running other SQL statements, like INSERT, DELETE and UPDATE follow a similar pattern:
+
+Constant for the SQL statement, with ? where dynamic values need to go
+Constant array for the params to substitute for each ? in the same order
+as the ?'s in the SQL constant
+Calling the appropriate database function with the SQL statement, params,
+and result handler function
+For non-SELECT statements, db.run() is the database function to use.
+The response handler function will pass through an error (err) if something
+goes wrong.
+
+Here's an example - notice the 4 question marks (?) match the 4 params.
+
+app.post('/trees', (req, res, next) => {
+   const sql = `
+    //      INSERT INTO trees (tree, location, height_ft, ground_circumference_ft)
+    //      VALUES (?, ?, ?, ?);
+     `;
+    const params = [
+        req.body.name,
+        req.body.location,
+        req.body.height,
+        req.body.size
+    ];
+
+    db.run(sql, params, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            res.json({
+                message: 'success'
+            });
+        }
+    });
+});
+`;
